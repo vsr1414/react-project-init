7213532618ad5bdbee004bb33ef5f27e87c7b814
vsr1414 <kamidivijayasimhareddy@gmail.com>
2026-01-30 14:31:14 +0530

    chore: initialize project with package.json and dependencies for React

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1", { id: "title" }, "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", { id: "title" }, "Hello World from React yo ho hooo!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
